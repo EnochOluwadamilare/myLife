249f8c05f5eb33a3435401dccb32362085c754ea
Oyerinde Enoch <oyerindeenoch@gmail.com>
2026-06-07 23:55:21 +0300

    feat(base): Constant config and routes setup

---
+++ src/constants/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  API_BASE_URL: "https://mla.elgabiz.com/api/v1",
+  APP_NAME: "myLife",
+  SYNC_INTERVAL: 300000,
+};
---
+++ src/constants/routes.ts
@@ -0,0 +1,21 @@
+export const ROUTES = {
+  Splash: "Splash",
+
+  OnboardingOne: "OnboardingOne",
+
+  OnboardingTwo: "OnboardingTwo",
+
+  Welcome: "Welcome",
+
+  Register: "Register",
+
+  Login: "Login",
+
+  SetupOne: "SetupOne",
+
+  SetupTwo: "SetupTwo",
+
+  SetupThree: "SetupThree",
+
+  Home: "Home",
+} as const;
